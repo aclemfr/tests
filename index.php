713494831283ad4576a602aaa8dba2be4f029f40
aclemfr <aclemfr@yahoo.fr>
2023-03-30 22:01:58 +0200

    2023 03 j30 22h00 -

--- index.php
+++ index.php
@@ -1,16 +1,42 @@
 <!DOCTYPE html>
 <html>
 
+
+<?php
+
+$nomPrenom = "Toto Man";
+$userAge = 17;
+
+?>
+
 <head>
     <meta charset="utf-8" />
     <title>Ma page web</title>
 </head>
 
 <!-- teston -->
+
 <body>
     <h1>Ma page web</h1>
     // toto tititit
     <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+
+    <p> 01 Ton Nom & Prénom = <?php echo $nomPrenom ?>. Ton age = <?php echo $userAge ?>.</p>
+
+    <?php echo "02 Ton Nom & Prénom = ";
+    echo $nomPrenom;
+    echo ". Ton age = ";
+    echo $userAge;
+    echo "."; ?>
+
+    <br><br>
+    <?php
+
+    $userAge *=  6;
+    echo "03 Ton Nom & Prénom =  $nomPrenom. Ton age = $userAge.";
+    ?>
+
+
 </body>
 
 
